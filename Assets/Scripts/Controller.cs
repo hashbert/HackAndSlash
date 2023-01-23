ffc105cee3a7a8cf927b017e82da84f8d66bc94a
hashbert <obert.chen@gmail.com>
2023-01-23 14:49:54 -0500

    Input and controller manager
    12A, 12B, 12C

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public int Index { get; private set; }
+    public bool IsAssigned { get; set; }
+    private string attackButton;
+    public bool attack;
+
+
+    private void Update()
+    {
+        if (!string.IsNullOrEmpty(attackButton))
+        {
+            attack = Input.GetButton(attackButton);
+        }
+    }
+
+    internal void SetIndex(int index)
+    {
+        Index = index; 
+        attackButton = "Attack" + Index;
+    }
+
+    internal bool AnyButtonDown()
+    {
+        return attack;
+    }
+}
---
+++ Assets/Scripts/ControllerManager.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class ControllerManager : MonoBehaviour
+{
+    private List<Controller> _controllers;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _controllers = FindObjectsOfType<Controller>().ToList();
+        int index = 1;
+        foreach(var controller in _controllers)
+        {
+            controller.SetIndex(index);
+            index++;
+        }
+    }
+
+    private void Update()
+    {
+        foreach(var controller in _controllers)
+        {
+            if (controller.IsAssigned == false && controller.AnyButtonDown())
+            {
+                AssignController(controller);
+            }
+        }
+    }
+
+    private void AssignController(Controller controller)
+    {
+        controller.IsAssigned = true;
+        Debug.Log("assigned controller " + controller.gameObject.name);
+    }
+}
